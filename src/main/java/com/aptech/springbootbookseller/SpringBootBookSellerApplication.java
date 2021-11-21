5098cc9a1daa23842f9f6d25359f0f32207c1b6a
tin <tinsatthu1988@gmail.com>
2021-11-21 19:09:12 +0700

    002 Configure Security - Authentication Manager and HttpSecurity

--- src/main/java/com/aptech/springbootbookseller/SpringBootBookSellerApplication.java
+++ src/main/java/com/aptech/springbootbookseller/SpringBootBookSellerApplication.java
@@ -8,13 +8,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringBootBookSellerApplication {
-
-	@Bean
-	public PasswordEncoder passwordEncoder()
-	{
-		return new BCryptPasswordEncoder();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBookSellerApplication.class, args);
 	}
