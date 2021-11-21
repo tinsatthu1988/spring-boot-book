b8f5d91af26cde136a6e782f095032d0b9eb3c7e
tin <tinsatthu1988@gmail.com>
2021-11-21 19:08:57 +0700

    001 Load User by Username - User Details Service

---
+++ src/main/java/com/aptech/springbootbookseller/security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.aptech.springbootbookseller.security;
+
+import com.aptech.springbootbookseller.model.User;
+import com.aptech.springbootbookseller.service.IUserService;
+import com.aptech.springbootbookseller.util.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private IUserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        Set<GrantedAuthority> authorities = Set.of(SecurityUtils.convertToAuthority(user.getRole().name()));
+
+        return UserPrincipal.builder()
+                .user(user).id(user.getId())
+                .username(username)
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+    }
+}
