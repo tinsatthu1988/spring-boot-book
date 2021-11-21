716f571657ae60133cdc33241789415df5f3ad4b
tin <tinsatthu1988@gmail.com>
2021-11-21 21:03:42 +0700

    002 JWT Helper Methods - Token Generation, Validation

---
+++ src/main/java/com/aptech/springbootbookseller/security/jwt/IJwtProvider.java
@@ -0,0 +1,14 @@
+package com.aptech.springbootbookseller.security.jwt;
+
+import com.aptech.springbootbookseller.security.UserPrincipal;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface IJwtProvider {
+    String generateToken(UserPrincipal auth);
+
+    Authentication getAuthentication(HttpServletRequest request);
+
+    boolean validateToken(HttpServletRequest request);
+}
